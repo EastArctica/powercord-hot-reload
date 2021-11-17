a95b69d9d84080ea6a76fae1b8a4f7de06ab1ad1
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-11-17 15:11:48 -0500

    refactor: move whitelist options into Whitelist category

--- Settings.jsx
+++ Settings.jsx
@@ -1,23 +1,30 @@
 const { React } = require("powercord/webpack");
-const { SwitchItem } = require("powercord/components/settings");
+const { SwitchItem, Category } = require("powercord/components/settings");
 
 module.exports = class Settings extends React.Component {
 	render() {
 		const { getSetting, toggleSetting } = this.props;
-
 		const plugins = Array.from(powercord.pluginManager.plugins.values());
 
 		return (
 			<div>
-				{plugins.map((plugin) => (
-					<SwitchItem
-						note={`${plugin.manifest.description} (${plugin.entityID})`}
-						value={getSetting(plugin.entityID, false)}
-						onChange={() => toggleSetting(plugin.entityID)}
-					>
-						{plugin.manifest.name}
-					</SwitchItem>
-				))}
+				<Category
+					name="Whitelist"
+					description="Choose which plugins you want to hot reload."
+					// could potentially cause issue if plugin was named 'whitelist'
+					opened={getSetting("whitelist", false)}
+					onChange={() => toggleSetting("whitelist")}
+				>
+					{plugins.map((plugin) => (
+						<SwitchItem
+							note={`${plugin.manifest.description} (${plugin.entityID})`}
+							value={getSetting(plugin.entityID, false)}
+							onChange={() => toggleSetting(plugin.entityID)}
+						>
+							{plugin.manifest.name}
+						</SwitchItem>
+					))}
+				</Category>
 			</div>
 		);
 	}
